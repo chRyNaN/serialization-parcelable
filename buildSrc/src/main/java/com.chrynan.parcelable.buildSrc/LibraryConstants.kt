f35fd7690d1596c984c99e3f607f0b0736bd5b03
Chris <ckeenan@starry.com>
2022-09-21 19:05:31 -0500

    Bumped version to 0.5.1 (9)

--- buildSrc/src/main/java/com.chrynan.parcelable.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.parcelable.buildSrc/LibraryConstants.kt
@@ -5,8 +5,8 @@ object LibraryConstants {
     const val group = "com.chrynan.parcelable"
     const val owner = "chrynan"
     const val repoName = "parcelable"
-    const val versionName = "0.5.0"
-    const val versionCode = 8
+    const val versionName = "0.5.1"
+    const val versionCode = 9
     const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/pacelable.git"
